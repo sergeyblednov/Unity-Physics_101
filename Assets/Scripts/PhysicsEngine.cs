1f7b7ceaa66c02a2fb8b79907c840a9f94771345
Sergey Blednov <sergey.blednov@pixacore.com>
2017-05-24 09:27:25 -0400

    Initial commit

---
+++ Assets/Scripts/PhysicsEngine.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[DisallowMultipleComponent]
+public class PhysicsEngine : MonoBehaviour {
+
+	public float mass;				// [kg]
+	public Vector3 velocityVector;	// [m s^-1]
+	public Vector3 netForceVector;	// N [kg m s^-2]
+	public bool showTrails = true;	
+
+	List<Vector3> forceVectorList = new List<Vector3>();
+	LineRenderer lineRenderer;
+	int numberOfForces;
+
+	// Use this for initialization
+	void Start () {
+		SetupTrails ();
+	}
+		
+	void FixedUpdate () {
+		RenderTrails ();
+		UpdatePosition ();
+	}
+
+	public void AddForce (Vector3 forceVector) {
+		forceVectorList.Add (forceVector);
+	}
+
+	void UpdatePosition () {
+		netForceVector = Vector3.zero;
+		foreach (Vector3 forceVector in forceVectorList) {
+			netForceVector += forceVector;
+		}
+
+		forceVectorList = new List<Vector3> ();
+
+		Vector3 accelerationVector = netForceVector / mass;
+		velocityVector += accelerationVector * Time.deltaTime;
+		transform.position += velocityVector * Time.deltaTime;
+	}
+
+	void SetupTrails () {
+		lineRenderer = gameObject.AddComponent<LineRenderer>();
+		lineRenderer.material = new Material(Shader.Find("Sprites/Default"));
+		//		lineRenderer.SetColors(Color.yellow, Color.yellow);
+		lineRenderer.startColor = Color.yellow;
+		lineRenderer.endColor = Color.yellow;
+		//		lineRenderer.SetWidth(0.2F, 0.2F);
+		lineRenderer.startWidth = 0.2f;
+		lineRenderer.endWidth = 0.2f;
+		lineRenderer.useWorldSpace = false;
+	}
+
+	void RenderTrails () {
+		if (showTrails) {
+			lineRenderer.enabled = true;
+			numberOfForces = forceVectorList.Count;
+			//			lineRenderer.SetVertexCount(numberOfForces * 2);
+			lineRenderer.positionCount = numberOfForces * 2;
+			int i = 0;
+			foreach (Vector3 forceVector in forceVectorList) {
+				lineRenderer.SetPosition(i, Vector3.zero);
+				lineRenderer.SetPosition(i+1, -forceVector);
+				i = i + 2;
+			}
+		} else {
+			lineRenderer.enabled = false;
+		}
+	}
+}
---
+++ Assets/Scripts/RocketEngine.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof(PhysicsEngine))]
+public class RocketEngine : MonoBehaviour {
+
+	public float fuelMass; 				// [kg]
+	public float maxThrust;				// kN [kg m s^-2]
+	[Range(0, 1f)]
+	public float thrustPercent;			// [none]
+	public Vector3 thrustUnitVector;	// [none]
+	PhysicsEngine physicsEngine;
+	float currentThrust;				// N
+
+	void Start () {
+		physicsEngine = GetComponent<PhysicsEngine> ();
+		physicsEngine.mass += fuelMass;
+	}
+
+	void FixedUpdate () {
+		if (fuelMass > FuelThisUpdate ()) {
+			fuelMass -= FuelThisUpdate ();
+			physicsEngine.mass -= FuelThisUpdate ();
+			ExertForce ();
+		} else {
+			Debug.LogWarning ("Out of rocket fuel");
+		}
+	}
+		
+	float FuelThisUpdate () {
+		float exhaustMassFlow;												// [kg s^-1]
+		float effectiveExhaustVelocity;										// [m s^-1]
+		effectiveExhaustVelocity = 4462f;
+		exhaustMassFlow = currentThrust / effectiveExhaustVelocity;			// [kg m s^-2 / m s^-1] = [kg s^-1]
+
+		return exhaustMassFlow * Time.deltaTime;							// [kg]
+	}
+
+	void ExertForce () {
+		currentThrust = thrustPercent * maxThrust * 1000f;
+		Vector3 thrustVector = thrustUnitVector.normalized * currentThrust;	// N
+		physicsEngine.AddForce (thrustVector);
+	}
+}
