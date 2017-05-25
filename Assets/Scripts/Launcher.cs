342f6a234dca006614053e211458c79a1ff9cdd1
Sergey Blednov <sergey.blednov@pixacore.com>
2017-05-25 10:16:15 -0400

    Making a simple game

---
+++ Assets/Scripts/Launcher.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Launcher : MonoBehaviour {
+
+	public float maxLaunchSpeed;
+	public AudioClip launchSound;
+	public AudioClip windupSound;
+	public PhysicsEngine ballToLaunch;
+
+	AudioSource audioSource;
+	public float launchSpeed;
+	float extraSpeedPerFrame;
+
+	void Start () {
+		audioSource = GetComponent<AudioSource> ();
+		audioSource.clip = windupSound;
+		extraSpeedPerFrame = (maxLaunchSpeed * Time.fixedDeltaTime) / audioSource.clip.length;
+	}
+
+	void OnMouseDown () {
+		launchSpeed = 0;
+		InvokeRepeating ("IncreaseLaunchSpeed", 0.5f, Time.fixedDeltaTime);
+		audioSource.clip = windupSound;
+		audioSource.Play ();
+	}
+
+	void OnMouseUp () {
+		audioSource.Stop ();
+		CancelInvoke ();
+		audioSource.clip = launchSound;
+		audioSource.Play ();
+
+		PhysicsEngine ball = Instantiate (ballToLaunch) as PhysicsEngine;
+		ball.transform.parent = GameObject.Find ("Launched Balls").transform;
+		Vector3 launchVelocity = new Vector3 (1, 1, 0).normalized * launchSpeed;
+		ball.velocityVector = launchVelocity;
+	}
+
+	void IncreaseLaunchSpeed () {
+		if (launchSpeed <= maxLaunchSpeed) {
+			launchSpeed += extraSpeedPerFrame;
+		}
+	}
+}
---
+++ Assets/Scripts/UniversalGravitation.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UniversalGravitation : MonoBehaviour {
+
+	public PhysicsEngine[] physicsEngineArray;
+	const float bigG = 6.67408e-11f;  // m^3⋅kg^−1⋅s^−2;
+
+	void Start () {
+		physicsEngineArray = GameObject.FindObjectsOfType<PhysicsEngine> ();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		CalculateGravity ();
+	}
+
+	void CalculateGravity () {
+		physicsEngineArray = GameObject.FindObjectsOfType<PhysicsEngine> ();
+		foreach (PhysicsEngine physicsEngineA in physicsEngineArray) {
+			foreach (PhysicsEngine physicsEngineB in physicsEngineArray) {
+				if (physicsEngineA != physicsEngineB ) {
+					Debug.Log ("Calculating force exerted on " + physicsEngineA.name +
+						" due to the gravity of " + physicsEngineB.name);
+
+					Vector3 offset = physicsEngineA.transform.position - physicsEngineB.transform.position;
+					float gravityMagnitude = bigG * physicsEngineA.mass * physicsEngineB.mass / Mathf.Pow (offset.magnitude, 2f);
+					Vector3 gravityVector = gravityMagnitude * offset.normalized;
+					physicsEngineB.AddForce (gravityVector);
+				}
+			}
+		}
+	}
+}
